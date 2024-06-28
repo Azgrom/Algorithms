e90acc212d968ede7219da2814606e7e9d60feab
Rafael Lúcio <rafaellucio@pm.me>
2024-06-28 17:06:17 -0300

    feat: finishing pq implementation

--- RustImplementation/src/data_structures/priority_queue.rs
+++ RustImplementation/src/data_structures/priority_queue.rs
@@ -1,5 +1,3 @@
-use std::collections::btree_map::Entry;
-use std::collections::btree_map::Entry::{Occupied, Vacant};
 use std::collections::BTreeMap;
 
 struct PriorityQueue<T> {
@@ -19,11 +17,12 @@ impl<T: Copy + Ord> From<&[T]> for PriorityQueue<T> {
         let mut priority_queue = Self::new(heap_size);
 
         for i in 0..heap_size {
-            priority_queue.map.insert(slice[i].clone(), i);
+            priority_queue.map_add(slice[i], i);
             priority_queue.heap.push(slice[i]);
         }
 
-        for i in usize::max(0, (heap_size / 2) - 1)..=0 {
+        let end = usize::max(0, (heap_size / 2) - 1);
+        for i in (0..=end).rev() {
             priority_queue.sink(i);
         }
 
@@ -151,11 +150,8 @@ impl<T: Copy + Ord> PriorityQueue<T> {
         }
 
         let new_heap_size = self.size() - 1; // TODO check if this should be decreased by one
-        let removed_data = if let Some(removed_data_reference) = self.heap.get(index) {
-            Some(removed_data_reference.clone())
-        } else {
-            None
-        };
+
+        let removed_data = self.heap.get(index).cloned();
         // let removed_data = self.heap.get(index);
         self.swap(index, new_heap_size);
 
@@ -167,22 +163,12 @@ impl<T: Copy + Ord> PriorityQueue<T> {
         }
 
         // let elem = self.heap.get(index);
-        let elem1 = if let Some(removed_data_reference) = self.heap.get(index) {
-            Some(removed_data_reference.clone())
-        } else {
-            None
-        };
+        let elem1 = self.heap.get(index).cloned();
 
         // Try sinking element
         self.sink(index);
 
-        let elem2 = if let Some(elem2) = self.heap.get(index) {
-            Some(elem2.clone())
-        } else {
-            None
-        };
-
-        if elem2 == elem1 {
+        if self.heap.get(index) == elem1.as_ref() {
             self.swim(index);
         }
 
@@ -198,10 +184,6 @@ impl<T: Copy + Ord> PriorityQueue<T> {
 
     fn map_remove(&mut self, value: &T, index: usize) {
         self.map.remove(value);
-    }
-
-    fn map_get(&mut self, value: T) -> T {
-        *self.map.entry(value).key()
     }
 
     fn map_swap(&mut self, i_elem: T, j_elem: T, i: usize, j: usize) {
@@ -287,6 +269,24 @@ mod tests{
     }
 
     #[test]
+    fn instantiation_from_invalid_heap_slice(){
+        let slice = &[1, 8, 5, 3, 6, 9];
+        let pq: PriorityQueue<i32> = slice[..].into();
+
+        assert_eq!(pq.size(), 6);
+        assert!(is_min_heap(0, &pq));
+    }
+
+    #[test]
+    fn instantiation_from_repeated_values(){
+        let slice = &[1; 6];
+        let pq: PriorityQueue<i32> = slice[..].into();
+
+        assert_eq!(pq.size(), 6);
+        assert!(is_min_heap(0, &pq));
+    }
+
+    #[test]
     fn poll_empty_queue() {
         let mut pq: PriorityQueue<i32> = PriorityQueue::new(1);
         assert_eq!(pq.poll(), None);
@@ -330,10 +330,95 @@ mod tests{
     }
 
     #[test]
+    fn is_min_heap_success() {
+        let mut pq = PriorityQueue::new(10);
+        pq.add(1);
+        pq.add(3);
+        pq.add(6);
+        pq.add(5);
+        pq.add(9);
+        pq.add(8);
+
+        assert!(is_min_heap(0, &pq));
+    }
+
+    #[test]
+    fn is_min_heap_failure_by_larger_left_leaf() {
+        let mut pq = PriorityQueue::new(6);
+        pq.heap = vec![1, 8, 5, 3, 6, 9]; // Manually creating an invalid heap
+        pq.map.insert(1, 0);
+        pq.map.insert(8, 5);
+        pq.map.insert(5, 1);
+        pq.map.insert(3, 2);
+        pq.map.insert(6, 3);
+        pq.map.insert(9, 4);
+
+        assert!(!is_min_heap(0, &pq));
+    }
+
+    #[test]
+    fn is_min_heap_failure_by_larger_right_leaf() {
+        let mut pq = PriorityQueue::new(6);
+        pq.heap = vec![1, 5, 1, 8, 3, 6, 9]; // Manually creating an invalid heap
+        pq.map.insert(1, 0);
+        pq.map.insert(5, 1);
+        pq.map.insert(1, 7);
+        pq.map.insert(8, 5);
+        pq.map.insert(3, 2);
+        pq.map.insert(6, 3);
+        pq.map.insert(9, 4);
+
+        assert!(!is_min_heap(0, &pq));
+    }
+
+    #[test]
     fn remove_nonexistent_element() {
         let mut pq: PriorityQueue<i32> = PriorityQueue::new(1);
         pq.add(42);
+        assert_eq!(pq.size(), 1);
         assert!(!pq.remove(99));
+        assert!(!pq.is_empty());
+        assert_eq!(pq.size(), 1);
+    }
+
+    #[test]
+    fn remove_existent_element() {
+        let mut pq: PriorityQueue<i32> = PriorityQueue::new(1);
+        pq.add(42);
+        assert_eq!(pq.size(), 1);
+        assert!(pq.remove(42));
+        assert!(pq.is_empty());
+        assert_eq!(pq.size(), 0);
+    }
+
+    #[test]
+    fn remove_element_maintains_heap_invariant() {
+        let mut pq: PriorityQueue<i32> = PriorityQueue::new(1);
+        pq.add(42);
+        assert!(is_min_heap(0, &pq));
+        assert!(pq.remove(42));
+        assert!(pq.is_empty());
+        assert!(is_min_heap(0, &pq));
+    }
+
+    #[test]
+    fn remove_where_right_element_is_smaller() {
+        let mut pq: PriorityQueue<i32> = PriorityQueue::new(7);
+        pq.heap = vec![42, 30, 43, 30, 31, 45, 44, 41];
+        pq.map.insert(42, 0);
+        pq.map.insert(30, 1);
+        pq.map.insert(43, 2);
+        pq.map.insert(30, 3);
+        pq.map.insert(31, 4);
+        pq.map.insert(45, 6);
+        pq.map.insert(44, 5);
+        pq.map.insert(41, 7);
+
+        assert_eq!(pq.size(), 8);
+        assert!(pq.remove(43));
+        assert!(!pq.is_empty());
+        assert!(is_min_heap(2, &pq));
+        assert!(is_min_heap(1, &pq));
     }
 
     #[test]
